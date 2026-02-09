707f9f1fca802309548d91c54f8b7c90fe5912fd
wasiahmad <wasiahmad@ucla.edu>
2026-02-09 15:01:45 -0800

    keeping bare minimum for eval harness
    Signed-off-by: wasiahmad <wasiahmad@ucla.edu>

--- swebench/harness/run_local_evaluation.py
+++ swebench/harness/run_local_evaluation.py
@@ -50,29 +50,7 @@ except Exception:
 import pandas as pd
 from tqdm import tqdm
 
-
-def get_dockerhub_image_uri(uid, dockerhub_username, repo_name=""):
-    repo_base, repo_name_only = repo_name.lower().split("/")
-    hsh = uid.replace("instance_", "")
-
-    if (
-        uid
-        == "instance_element-hq__element-web-ec0f940ef0e8e3b61078f145f34dc40d1938e6c5-vnan"
-    ):
-        repo_name_only = "element-web"  # Keep full name for this one case
-    elif "element-hq" in repo_name.lower() and "element-web" in repo_name.lower():
-        repo_name_only = "element"
-        if hsh.endswith("-vnan"):
-            hsh = hsh[:-5]
-    # All other repos: strip -vnan suffix
-    elif hsh.endswith("-vnan"):
-        hsh = hsh[:-5]
-
-    tag = f"{repo_base}.{repo_name_only}-{hsh}"
-    if len(tag) > 128:
-        tag = tag[:128]
-
-    return f"{dockerhub_username}/sweap-images:{tag}"
+from swebench.harness.image_uri import get_dockerhub_image_uri
 
 
 # Credit: prabhuteja12
